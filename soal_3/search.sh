3c999b63a107dc2390127406fb2ad4a8344af014
ItunyaKuda <michael.kenneth.salim@gmail.com>
2024-03-25 23:24:49 +0700

    cari

--- soal_3/search.sh
+++ soal_3/search.sh
@@ -3,13 +3,23 @@
 cd genshin_character
 for folder in *;
 do
- echo "$folder"
- for file in $folder/*.jpg;
+ for file in $folder/*;
   do
   steghide extract -sf "$file" -xf "$file.txt" -p "" -q
-  
+  rahasia=$(cat "$file.txt")
+  waktu=$(date +"%d/%m/%y %H:%M:%S")
+  alamat=$(realpath "$file.txt")
 
+  if [[ $rahasia == 68747470 ]];
+  then
+  echo "[$waktu] [FOUND] [$alamat]"
+  exit 
+  else
+  echo "[$waktu] [NOT FOUND] [$alamat]" 
+  rm "$file.txt"
+  fi >> image.log
 
   sleep 1
   done
 done
+  mv "image.log" ".." 
